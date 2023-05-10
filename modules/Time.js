83de360e5ea855cfa881f272c474d898e152c5fd
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-05-10 12:11:58 -0500

    Fix
    Fix linters errors

--- modules/Time.js
+++ modules/Time.js
@@ -1,6 +1,7 @@
+/* eslint-disable import/prefer-default-export */
 export const gettime = (() => {
-    const time = document.querySelector('.date');
-    const ctimep = document.createElement('p');
-    ctimep.textContent = 'hola';
-    time.append(ctimep);
+  const time = document.querySelector('.date');
+  const ctimep = document.createElement('p');
+  ctimep.textContent = 'hola';
+  time.append(ctimep);
 });
